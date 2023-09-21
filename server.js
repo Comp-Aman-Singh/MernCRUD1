673a10708aadbd991e9308b6a734128a73791fc0
Comp-Aman-Singh <singhaman143007@gmail.com>
2023-09-21 18:49:55 +0530

    Day1 Commit
    This was the day 1 for the crud application

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-//calling env variable
-if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
-}
-
 //Import expresss
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
+const Note = require("./models/note.js");
 
+//calling env variable
+require("dotenv").config();
 
 //connecting DB
+connectToDb();
 
 //creating the express app
 const app = express();
-connectToDb();
 
 //Routing in the app
-
 app.get("/", (req, res) => {
   res.json({ hello: "hel" });
+});
+
+//configure Express
+app.use(express.json());
+
+app.post("/notes", async (req, res) => {
+  console.log("notes");
+  // get the sent in data off request body
+
+  const title = req.body.title;
+  const body = req.body.body;
+  try {
+    const note = await Note.create({
+      title: title,
+      body: body,
+    });
+    res.json({ note: note });
+    console.log("The request has been sent ");
+  } catch (err) {
+    console.log("Internal server error", err);
+  }
 });
 
 //starting the sever
